ca140e9d1f3a515b9379da98af08ae3abd23248e
Jim Hommes <jim@homm.es>
2021-03-18 16:45:01 +0100

    Marginal cost is calculated

--- emlab/capacity_market.py
+++ emlab/capacity_market.py
@@ -14,4 +14,17 @@ db_url = sys.argv[1]
 db = SpineDB(db_url)
 db_data = db.export_data()
 
+# Create Objects from the DB Data in the Repository
 reps = Repository(db_data)
+
+# For every energy producer we will submit bids to the Capacity Market
+for energyProducer in reps.energyProducers.values():
+
+    # For every plant owned by energyProducer
+    for powerPlant in reps.get_powerplants_by_owner(energyProducer.name):
+        # Calculate marginal cost mc
+        #   fuelConsumptionPerMWhElectricityProduced = 3600 / (pp.efficiency * ss.energydensity)
+        #   lastKnownFuelPrice
+        substances = reps.get_substances_by_powerplant(powerPlant.name)
+        if len(substances) > 0:         # Only done for 1 substance atm
+            mc = 3600 / (int(powerPlant.parameters['Efficiency']) * substances[0].parameters['energyDensity'])
--- emlab/repository.py
+++ emlab/repository.py
@@ -1,37 +1,54 @@
+def db_objects_to_dict(db_data, to_dict, object_class_name, class_to_create):
+    for unit in [i for i in db_data['objects'] if i[0] == object_class_name]:
+        to_dict[unit[1]] = class_to_create(unit)
+
+    for unit in to_dict.values():
+        for parameterValue in [i for i in db_data['object_parameter_values'] if i[1] == unit.name]:
+            unit.add_parameter_value(parameterValue)
+
+
+def db_relationships_to_arr(db_data, to_arr, relationship_class_name):
+    for unit in [i for i in db_data['relationships'] if i[0] == relationship_class_name]:
+        to_arr.append(unit[1], unit[2])
+
+
+class ImportObject:
+    def __init__(self, import_obj):
+        self.name = import_obj[1]
+        self.parameters = {}
+
+    def add_parameter_value(self, import_obj):
+        self.parameters[import_obj[2]] = import_obj[3]
+
 
 class Repository:
     def __init__(self, db_data):
         self.energyProducers = {}
         self.powerPlants = {}
+        self.substances = {}
+        self.powerPlantsFuelMix = []
+        self.tempFixedFuelPrices = {'biomass': 10, 'fuelOil': 20, 'hardCoal': 30, 'ligniteCoal': 10, 'naturalGas': 5,
+                                    'uranium': 40}
 
-        for energyProducer in [i for i in db_data['objects'] if i[0] == 'EnergyProducers']:
-            self.energyProducers[energyProducer[1]] = EnergyProducer(energyProducer)
+        db_objects_to_dict(db_data, self.energyProducers, 'EnergyProducers', EnergyProducer)
+        db_objects_to_dict(db_data, self.powerPlants, 'PowerPlants', PowerPlant)
+        db_objects_to_dict(db_data, self.substances, 'Substances', Substance)
+        db_relationships_to_arr(db_data, self.powerPlantsFuelMix, 'PowerGeneratingTechnologyFuel')
 
-        for energyProducer in self.energyProducers:
-            for parameterValue in [i for i in db_data['object_parameter_values'] if i[1] == energyProducer.name]:
-                energyProducer.add_parameter_value(parameterValue)
+    def get_powerplants_by_owner(self, owner):
+        return [i for i in self.powerPlants if i.parameters['Owner'] == owner]
 
-        for powerPlant in [i for i in db_data['objects'] if i[0] == 'PowerPlants']:
-            self.powerPlants[powerPlant[1]] = PowerPlant(powerPlant)
-
-        for powerPlant in self.powerPlants:
-            for parameterValue in [i for i in db_data['object_parameter_values'] if i[1] == powerPlant.name]:
-                powerPlant.add_parameter_value(parameterValue)
-
-
-class EnergyProducer:
-    def __init__(self, import_obj):
-        self.name = import_obj[1]
-        self.parameters = {}
-
-    def add_parameter_value(self, import_obj):
-        self.parameters[import_obj[2]] = import_obj[3]
+    def get_substances_by_powerplant(self, powerplant_name):
+        return [self.substances[i] for i in self.powerPlantsFuelMix if i[0] == powerplant_name]
 
 
-class PowerPlant:
-    def __init__(self, import_obj):
-        self.name = import_obj[1]
-        self.parameters = {}
+class EnergyProducer(ImportObject):
+    pass
 
-    def add_parameter_value(self, import_obj):
-        self.parameters[import_obj[2]] = import_obj[3]
+
+class PowerPlant(ImportObject):
+    pass
+
+
+class Substance(ImportObject):
+    pass
