ae300d00e648f58c2f2f98f499ff5ac53da46e34
Jim Hommes <jim@homm.es>
2021-03-18 17:19:25 +0100

    Added link through PowerGeneratingTechnology and bugfixes

--- emlab/repository.py
+++ emlab/repository.py
@@ -31,18 +31,22 @@ class Repository:
                                     'uranium': 40}
         self.electricitySpotMarkets = {}
         self.powerPlantDispatchPlans = []
+        self.powerGeneratingTechnologies = {}
 
         db_objects_to_dict(db_data, self.energyProducers, 'EnergyProducers', EnergyProducer)
         db_objects_to_dict(db_data, self.powerPlants, 'PowerPlants', PowerPlant)
         db_objects_to_dict(db_data, self.substances, 'Substances', Substance)
         db_relationships_to_arr(db_data, self.powerPlantsFuelMix, 'PowerGeneratingTechnologyFuel')
         db_objects_to_dict(db_data, self.electricitySpotMarkets, 'ElectricitySpotMarkets', ElectricitySpotMarket)
+        db_objects_to_dict(db_data, self.powerGeneratingTechnologies, 'PowerGeneratingTechnologies',
+                           PowerGeneratingTechnology)
 
     def get_powerplants_by_owner(self, owner):
         return [i for i in self.powerPlants.values() if i.parameters['Owner'] == owner]
 
     def get_substances_by_powerplant(self, powerplant_name):
-        return [self.substances[i[1]] for i in self.powerPlantsFuelMix if i[0] == powerplant_name]
+        return [self.substances[i[1]] for i in self.powerPlantsFuelMix if i[0] == self.powerPlants[powerplant_name]
+                .parameters['Technology']]
 
     def create_powerplant_dispatch_plan(self, plant, bidder, bidding_market, amount, price):
         ppdp = PowerPlantDispatchPlan()
@@ -67,6 +71,10 @@ class Substance(ImportObject):
 
 
 class ElectricitySpotMarket(ImportObject):
+    pass
+
+
+class PowerGeneratingTechnology(ImportObject):
     pass
 
 
