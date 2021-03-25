34e667ff340dbdc9689a5914dc297e48651d5133
Jim Hommes <jim@homm.es>
2021-03-25 13:29:54 +0100

    Initialized CapacityMarket py

---
+++ emlab/capacitymarket_submitbids.py
@@ -0,0 +1,11 @@
+#
+# The file responsible for submitting bids to the capacity market.
+#
+# Jim Hommes - 25-3-2021
+#
+
+
+class CapacityMarketSubmitBids:
+
+    def __init__(self, reps):
+        self.reps = reps
