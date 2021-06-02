e99e866f07fd0f4537cc9af012e67e2214dd31d3
Jim Hommes <jim@homm.es>
2021-06-02 16:12:01 +0200

    Successfully imported type 1 mapping into ms access

--- resources/scripts/competes_spinedb_to_ms_access.py
+++ resources/scripts/competes_spinedb_to_ms_access.py
@@ -9,6 +9,57 @@ import shutil
 import sys
 from spinedb import SpineDB
 
+
+def escape_parameter(parameter):
+    to_escape = ['ORDER']
+
+    if parameter.upper() in to_escape:
+        return '[' + parameter + ']'
+    else:
+        return parameter
+
+
+def export_to_mdb(path: str, filename: str, type1: dict):
+    print('Initializing connection to ' + filename)
+    try:
+        con_string = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=' + path + filename + ';'
+        conn = pyodbc.connect(con_string)
+        cursor = conn.cursor()
+        print("Connected To " + filename)
+
+        print('Staging Type 1 Mappings...')
+        for table in type1.keys():
+            export_type1(cursor, table, type1[table])
+
+        print('Committing...')
+        cursor.commit()
+        cursor.close()
+        conn.close()
+        print('Done')
+
+    except pyodbc.Error as e:
+        print("Error in Connection", e)
+
+
+def export_type1(cursor, table_name, id_param):
+    for (_, unique_param, _) in [i for i in db_competes_data['objects'] if i[0] == table_name]:
+        param_names = []
+        param_values = []
+        for (_, _, param_name, param_value, _) in [i for i in db_competes_data['object_parameter_values']
+                                                   if i[0] == table_name and i[1] == unique_param]:
+            param_names.append(param_name)
+            if type(param_value) is str:
+                param_values.append(param_value)
+            else:
+                param_values.append(str(param_value))
+        sql_statement = 'INSERT INTO [' + table_name + '] (['+id_param+'], ' + ', '.join(['[' + str(i) + ']' for i in param_names]) + \
+                        ") VALUES (?, " + ', '.join(['?' for i in param_values]) + ');'
+        values = (unique_param,) + tuple(param_values)
+        print(values)
+        print(sql_statement.replace('?', '%s') % values)
+        cursor.execute(sql_statement, values)
+
+
 print('===== Starting COMPETES SpineDB to MS Access script =====')
 
 print('Copying empty databases...')
@@ -22,36 +73,27 @@ print('Done copying empty databases')
 print('Connecting and exporting SpineDB...')
 db_competes = SpineDB(sys.argv[1])
 db_competes_data = db_competes.export_data()
+db_competes.close_connection()
 print('Done')
 
-print('Initializing connection to COMPETES EU 2050-KIP.mdb')
-try:
-    con_string = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=E:\Dropbox\workspace\Spine_EMLab_COMPETES\COMPETES\Data\COMPETES EU 2050-KIP.mdb;'
-    conn = pyodbc.connect(con_string)
-    cursor = conn.cursor()
-    print("Connected To Database")
+export_to_mdb(r'E:\Dropbox\workspace\Spine_EMLab_COMPETES\COMPETES\Data\\', 'COMPETES EU 2050-KIP.mdb',
+              {'BusCountry': 'Bus',
+               'Country': 'Country',
+               'FuelGen': 'FUELGEN',
+               'FuelType': 'FUELNEW',
+               'Input_Years': 'Input Year',
+               'Months': 'MonthDef',
+               'Season': 'SeasonInput',
+               'Techtype': 'FUELTYPENEW',
+               'VRE Technologies': 'Technology'})
 
-    id_param_names = {'BusCountry': 'Bus'}
-
-    print('Adding some values...')
-    for (table_name, unique_param, _) in [i for i in db_competes_data['objects'] if i[0]=='BusCountry']:
-        param_names = []
-        param_values = []
-        for (_, _, param_name, param_value, _) in [i for i in db_competes_data['object_parameter_values']
-                                                   if i[0] == table_name and i[1] == unique_param]:
-            param_names.append(param_name)
-            if type(param_value) is str:
-                param_values.append("'" + param_value + "'")
-            else:
-                param_values.append(str(param_value))
-        sql_statement = 'INSERT INTO BusCountry (' + id_param_names['BusCountry'] + ', ' + ', '.join([str(i) for i in param_names]) + ") VALUES ('" + unique_param + "', " + ', '.join(param_values) + ');'
-        print(sql_statement)
-        cursor.execute(sql_statement)
-    cursor.commit()
-    cursor.close()
-    conn.close()
-
-except pyodbc.Error as e:
-    print("Error in Connection", e)
+type1pp = {'Installed Capacity Abroad': '',
+           'Installed Capacity-RES Abroad': '',
+           'NL Installed Capacity (+heat)': '',
+           'NL Installed Capacity-RES (+he': ''}
 
 print('===== End of COMPETES SpineDB to MS Access script =====')
+
+
+
+
