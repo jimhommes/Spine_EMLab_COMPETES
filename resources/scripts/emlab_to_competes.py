6fc280f0c5f3dfad7f50091fd05a2691c5d69904
Jim Hommes <jim@homm.es>
2021-06-11 12:19:47 +0200

    Part 1 in including revenues as CAPEX costs

--- resources/scripts/emlab_to_competes.py
+++ resources/scripts/emlab_to_competes.py
@@ -1,11 +1,11 @@
 """
 This script handles all communication passed from the EM-Lab SpineDB to the COMPETES SpineDB.
-As of this moment this only entails the CO2 Price.
+As of this moment this only entails the CO2 Price and Capacity Revenues.
 
 Jim Hommes - 1-6-2021
 """
 import sys
-
+import numpy as np
 from spinedb import SpineDB
 
 print('===== Starting EMLab to COMPETES script =====')
@@ -14,15 +14,18 @@ print('Loading Databases...')
 db_emlab = SpineDB(sys.argv[1])
 db_emlab_data = db_emlab.export_data()
 db_competes = SpineDB(sys.argv[2])
+db_competes_data = db_competes.export_data()
 print('Done loading Databases.')
 
 print('Loading current EM-Lab tick...')
 current_emlab_tick = max(
     [int(i[3]) for i in db_emlab_data['object_parameter_values'] if i[0] == i[1] == 'SystemClockTicks' and
      i[2] == 'ticks'])
-current_competes_tick = 2020 + round(current_emlab_tick / 5) * 5
+current_competes_tick_rounded = 2020 + round(current_emlab_tick / 5) * 5
+current_competes_tick = 2020 + current_emlab_tick
 print('Current EM-Lab tick is ' + str(current_emlab_tick) +
-      ', which translates to COMPETES tick ' + str(current_competes_tick))
+      ', which translates to COMPETES tick ' + str(current_competes_tick) + ' or rounded '
+      + str(current_competes_tick_rounded))
 
 print('Creating structure...')
 object_class_name = 'EU_ETS_CO2price'
@@ -42,11 +45,51 @@ mcp = next(float(i[3]) for i in mcp_lines if i[1] == mcp_object and i[2] == 'Pri
 print('Price found: ' + str(mcp))
 
 print('Staging prices...')
-db_competes.import_object_parameter_values([(object_class_name, str(current_competes_tick), i, mcp) for i in parameters])
+db_competes.import_object_parameter_values([(object_class_name, str(current_competes_tick), i, mcp, '0') for i in parameters])
+
+if current_emlab_tick > 0:
+    print('Getting CapacityMarket clearing price...')
+    capacity_market_clearing_price = next(float(i[3]) for i in db_emlab_data['object_parameter_values'] if i[0] == 'MarketClearingPoints' and i[2] == 'Price' and i[4] == str(current_emlab_tick - 1))
+    print('Capacity Market Clearing Price found: ' + str(capacity_market_clearing_price))
+
+    print('Aggregating Capacity Market revenues per technology...')
+    technology_total_revenue = {}
+    for ppdp_object in [i[1] for i in db_emlab_data['objects'] if i[0] == 'PowerPlantDispatchPlans']:
+        values = {i[2]: i[3] for i in db_emlab_data['object_parameter_values'] if i[0] == 'PowerPlantDispatchPlans' and i[1] == ppdp_object and i[4] == str(current_emlab_tick-1)}
+        if 'Market' in values.keys() and values['Market'] == 'DutchCapacityMarket':
+            unit = values['Plant']
+            tech = next(i[3] for i in db_emlab_data['object_parameter_values'] if i[0] == 'PowerPlants' and i[1] == unit and i[2] == 'TECHTYPENL')
+            revenue = float(values['AcceptedAmount']) * capacity_market_clearing_price
+            try:
+                technology_total_revenue[tech] += revenue
+            except KeyError:
+                technology_total_revenue[tech] = revenue
+
+    print(technology_total_revenue)
+    print('Done')
+
+    print('Staging revenues as CAPEX reductions...')
+    print('Part 1: Overnight costs')
+    for tech in technology_total_revenue.keys():
+        valuemap_per_fuel = next(i[3] for i in db_competes_data['object_parameter_values'] if i[0] == 'Overnight Cost (OC)' and i[1] == tech)
+        param_name = next(i[2] for i in db_competes_data['object_parameter_values'] if i[0] == 'Overnight Cost (OC)' and i[1] == tech)
+        print(param_name)
+        for valuemap_per_year in valuemap_per_fuel.values:
+            i = valuemap_per_year.indexes.tolist().index(str(current_competes_tick))
+            print('Old value: ' + str(valuemap_per_year.values[i]))
+            valuemap_per_year.values[i] -= technology_total_revenue[tech]
+            print('New value: ' + str(valuemap_per_year.values[i]))
+
+        db_emlab.import_object_parameter_values([('Overnight Cost (OC)', tech, param_name, valuemap_per_fuel.to_dict(), '0')])
+
+    print('Done')
 
 print('Committing...')
-db_competes.commit('Committing CO2 Prices. EMLab tick: ' + str(current_emlab_tick) +
+db_competes.commit('Committing CO2 Prices and Revenues. EMLab tick: ' + str(current_emlab_tick) +
                    ', COMPETES tick: ' + str(current_competes_tick))
+
+db_competes.close_connection()
+db_emlab.close_connection()
 print('Done!')
 print('===== End of EMLab to COMPETES script =====')
 
